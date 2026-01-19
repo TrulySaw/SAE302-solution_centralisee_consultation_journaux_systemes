a94ccd1976438ae2d7b5a4375d5c35b406fc4ea1
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-19 14:07:50 +0100

    Update app.run to use port from configuration file
    Read port configuration from a file and use it to run the app.

--- sae_302/run.py
+++ sae_302/run.py
@@ -2,6 +2,9 @@ from app import create_app
 
 # creation de l'application flask
 app = create_app()
+with open("./config/ports", "r") as p:
+    port = p.readlines().strip()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=port)
+    
