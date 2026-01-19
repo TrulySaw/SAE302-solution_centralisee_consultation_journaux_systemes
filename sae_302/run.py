8aa8a6567cab4617d2c64a40edfee64129aba53c
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-19 14:12:48 +0100

    Clarify app creation comment in run.py
    Updated comment to clarify the app creation process.

--- sae_302/run.py
+++ sae_302/run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-# creation de l'application flask
+# creation de l'application flask et récupération du port
 app = create_app()
 with open("./app/config/ports", "r") as p:
     port = p.readline().strip()
