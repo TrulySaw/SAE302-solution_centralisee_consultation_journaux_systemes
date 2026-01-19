5f363e5354f17a0fcf08dfb6faf5b17f3e646de8
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-19 14:12:37 +0100

    Update port configuration file path

--- sae_302/run.py
+++ sae_302/run.py
@@ -2,8 +2,8 @@ from app import create_app
 
 # creation de l'application flask
 app = create_app()
-with open("./config/ports", "r") as p:
-    port = p.readlines()[0].strip()
+with open("./app/config/ports", "r") as p:
+    port = p.readline().strip()
 
 if __name__ == '__main__':
     app.run(debug=True, port=port)
