7dbad5890b0ccb912ea1746048bea09ebc2ef0b6
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-19 14:09:29 +0100

    Fix port reading from configuration file

--- sae_302/run.py
+++ sae_302/run.py
@@ -3,7 +3,7 @@ from app import create_app
 # creation de l'application flask
 app = create_app()
 with open("./config/ports", "r") as p:
-    port = p.readlines().strip()
+    port = p.readlines()[0].strip()
 
 if __name__ == '__main__':
     app.run(debug=True, port=port)
