fb4c633b415cc593a43838bf5350ac5c41798e0f
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-20 18:03:03 +0100

    Update log_check.py

--- sae_302/app/log_check.py
+++ sae_302/app/log_check.py
@@ -15,13 +15,10 @@ def priv(required_priv):
     Récupération des privilèges de l'utilisateur si connecté, en fonction de son rôle
     """
     try:
-        if is_log():
-            priv_user = session.get("privilege")
-            if required_priv == 1:
-                return priv_user >= 1
-            if required_priv == 2:
-                return priv_user >= 3
-            if required_priv == 4:
-                return priv_user == 7
+        if is_log():     
+            if session.get("privilege") & required_priv:
+                return True
+        else:
+            return app.redirect("/login")
     except:
         return False
