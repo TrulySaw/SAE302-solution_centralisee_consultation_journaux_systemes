2cc94e26f7bf8254d459e9163b2cc8de9b2c96dd
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-20 19:28:26 +0100

    Update run.py

--- sae_302/run.py
+++ sae_302/run.py
@@ -1,10 +1,9 @@
 from app import create_app
 
-# creation de l'application flask et récupération du port
+# creation de l'application flask, et récupération du port
 app = create_app()
-with open("./app/config/ports", "r") as p:
+with open("./app/config/port", "r") as p:
     port = p.readline().strip()
 
 if __name__ == '__main__':
     app.run(debug=True, port=port)
-    
