54e6ca20c21b9d8838380d8fba0712705d8f26d8
Saw <126684041+TrulySaw@users.noreply.github.com>
2026-01-22 12:27:51 +0100

    Fix comment formatting in run.py

--- sae_302/run.py
+++ sae_302/run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-# creation de l'application flask, et récupération du port
+# création de l'application flask, et récupération du port
 app = create_app()
 with open("./app/config/port", "r") as p:
     port = p.readline().strip()
